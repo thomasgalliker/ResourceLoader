d3f645914c5403d92e2a81ca32de9f423ff8108f
thomasgalliker <thomas@bluewin.ch>
2016-04-21 11:24:15 +0200

    Add sample project (android, ios, windows phone) for ResourceLoader

---
+++ Samples/ResourceLoaderSample/ResourceLoaderSample/App.cs
@@ -0,0 +1,40 @@
+﻿using System.Reflection;
+
+using Xamarin.Forms;
+
+namespace ResourceLoaderSample
+{
+    public class App : Application
+    {
+        public App()
+        {
+            var xmlContent = ResourceLoader.Current.GetEmbeddedResourceString(this.GetType().GetTypeInfo().Assembly, "XMLFile1.xml");
+
+            // The root page of your application
+            this.MainPage = new ContentPage
+                                {
+                                    Content =
+                                        new StackLayout
+                                            {
+                                                VerticalOptions = LayoutOptions.Center,
+                                                Children = { new Label { XAlign = TextAlignment.Center, Text = xmlContent } }
+                                            }
+                                };
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
