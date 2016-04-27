13e4d4d217ebc28f8f6f483e17f6fb726e450946
thomasgalliker <thomas@bluewin.ch>
2016-04-27 12:40:29 +0200

    Add sample project for Netfx

---
+++ Samples/ResourceLoaderSample.Netfx/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Reflection;
+
+namespace ResourceLoaderSample.Netfx
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var resourceAssembly = Assembly.Load("ResourceLoaderSample");
+            string xmlFilePath = "XMLFile1.xml";
+
+            string xmlContent = ResourceLoader.Current.GetEmbeddedResourceString(resourceAssembly, xmlFilePath);
+
+            Console.WriteLine(xmlContent);
+            Console.ReadLine();
+        }
+    }
+}
