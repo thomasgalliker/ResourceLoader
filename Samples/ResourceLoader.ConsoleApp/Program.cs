98e5e47b3530dd5e3ef53c6e32262dc303d7cea7
thomasgalliker <thomas@bluewin.ch>
2025-12-10 07:51:59 +0100

    Add sample app

---
+++ Samples/ResourceLoader.ConsoleApp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ResourceLoader.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
