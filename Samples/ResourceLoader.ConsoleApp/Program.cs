660cffa6785478094886ce224bbb0bb3753856f2
thomasgalliker <thomas@bluewin.ch>
2025-12-10 08:03:01 +0100

    Update sample app

--- Samples/ResourceLoader.ConsoleApp/Program.cs
+++ Samples/ResourceLoader.ConsoleApp/Program.cs
@@ -1,10 +1,23 @@
-﻿namespace ResourceLoader.ConsoleApp
+﻿using System.Reflection;
+
+namespace ResourceLoader.ConsoleApp
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine(
+                $"ResourceLoader Sample App{Environment.NewLine}" +
+                $"Copyright(C) superdev GmbH. All rights reserved.{Environment.NewLine}");
+
+            var assembly = typeof(Program).Assembly;
+            var resourceName = "XMLFile1.xml";
+
+            var content = IResourceLoader.Current.GetEmbeddedResourceString(assembly, resourceName);
+
+            Console.WriteLine($"IResourceLoader.Current.GetEmbeddedResourceString({assembly.GetName().Name}, {resourceName}) returned:");
+            Console.WriteLine(content);
+            Console.ReadLine();
         }
     }
 }
